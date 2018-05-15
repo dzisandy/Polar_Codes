f0e0288291578b30fb5f9f95f11247fc6b39988a
Andrei Dzis <dzisandy@mail.ru>
2018-05-15 15:47:24 +0300

    edited Poster

--- Polar Codes/GM.m
+++
@@ -1,5 +0,0 @@
-sigma = 0.5;
-obj1 = gmdistribution([0 2]', cat(3, sigma^2, sigma^2), [0.5 0.5]);
-Y1 = random(obj1, 1e4);
-res = log( (exp((4*Y1 - 4)./2*sigma^2)+1)./(exp((-4*Y1 - 4)./2*sigma^2)+1) );
-hist(res, -3:0.01:5)
